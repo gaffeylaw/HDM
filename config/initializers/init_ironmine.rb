33174b56029b7b347fc671223850f6d47c31ac5d
hexj <xiangjia.he@hand-china.com>
2014-08-24 19:39:52 +0800

    delete some useless content

--- config/initializers/init_ironmine.rb
+++ config/initializers/init_ironmine.rb
@@ -2,4 +2,5 @@ begin
   ActiveRecord::Base.send(:include,Dip::NotInsertNil)
   DB_ADAPTER=Ironmine::Application.config.database_configuration[Rails.env]["adapter"].to_s.upcase
   DB_OWNER= Ironmine::Application.config.database_configuration[Rails.env]["username"].to_s.upcase
+
 end
