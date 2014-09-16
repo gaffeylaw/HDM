50846311856892b78c181c10ee706fcb8262ec2c
hexj <xiangjia.he@hand-china.com>
2014-09-16 16:30:58 +0800

    fix bug

--- modules/120_epm_dataimport_dip/db/migrate/20140915073859_create_dip_organization.rb
+++ modules/120_epm_dataimport_dip/db/migrate/20140915073859_create_dip_organization.rb
@@ -12,7 +12,7 @@ class CreateDipOrganization < ActiveRecord::Migration
       t.timestamps
     end
     change_column :dip_organization, "id", :string, :limit => 22
-    execute %{create or replace view dip_organization_v as select t.rowid row_id,t.* from dip_organization t }
+    execute %{select t.rowid row_id,t."ID" "ORG_ID",t."ORG_NAME",t."P_ORG_ID",t."P_ORG_NAME",t."CREATED_BY",t."UPDATED_BY",t."COMBINATION_RECORD",t."BATCH_ID",t."IDX",t."CREATED_AT",t."UPDATED_AT" from dip_organization t}
     add_index(:dip_organization,:p_org_id,:name=>:dip_organization_i01)
   end
 
