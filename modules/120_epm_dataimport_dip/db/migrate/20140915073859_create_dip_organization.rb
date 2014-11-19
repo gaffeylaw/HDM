d40e5a649e7f22bd13828ae450d26ab60813d17e
hxj339449 <339449829@qq.com>
2014-11-19 17:23:10 +0800

    Update 20140915073859_create_dip_organization.rb

--- modules/120_epm_dataimport_dip/db/migrate/20140915073859_create_dip_organization.rb
+++ modules/120_epm_dataimport_dip/db/migrate/20140915073859_create_dip_organization.rb
@@ -1,6 +1,7 @@
 class CreateDipOrganization < ActiveRecord::Migration
   def change
     create_table :dip_organization do |t|
+      t.string :org_id,:limit=>22,:nill=>false
       t.string :org_name,:limit=>150, :nil=>false
       t.string :p_org_id,:limit=>22
       t.string :p_org_name,:limit=>150
@@ -12,7 +13,7 @@ class CreateDipOrganization < ActiveRecord::Migration
       t.timestamps
     end
     change_column :dip_organization, "id", :string, :limit => 22
-    execute %{create or replace view dip_organization_v as select t.rowid row_id,t."ID" "ORG_ID",t."ORG_NAME",t."P_ORG_ID",t."P_ORG_NAME",t."CREATED_BY",t."UPDATED_BY",t."COMBINATION_RECORD",t."BATCH_ID",t."IDX",t."CREATED_AT",t."UPDATED_AT" from dip_organization t}
+    execute %{create or replace view dip_organization_v as select t.rowid row_id,t."ORG_ID" "ORG_ID",t."ORG_NAME",t."P_ORG_ID",t."P_ORG_NAME",t."IDX",t."CREATED_BY",t."UPDATED_BY",t."COMBINATION_RECORD",t."BATCH_ID",t."CREATED_AT",t."UPDATED_AT" from dip_organization t}
     add_index(:dip_organization,:p_org_id,:name=>:dip_organization_i01)
   end
 
